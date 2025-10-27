2b1c6957ab2171ebfdb77b310244cffbb579b103
lucas-dm <lucas.dm@kakaoenterprise.com>
2025-10-27 11:53:12 +0900

    chore: edit chores

--- internal/acctest/provider_test.go
+++ internal/acctest/provider_test.go
@@ -3,15 +3,16 @@
 package acctest
 
 import (
+	"terraform-provider-kakaocloud/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"terraform-provider-kakaocloud/internal/provider"
 )
 
 const (
 	providerConfig = `
 	provider "kakaocloud" {
-	x_auth_token =  "gAAAAABohzSLs3PCvr1ytmHl9wNtVbllQtt5C8tIsXsqf2qU3sChzr2So8P6TCGnjDegpby1GAsCy0tR8Aqhy35Ip7U64s734nsEk15MbvsX9MGba08-aH2Ho8wkbpLhi7sL2k0eX6bzLCtbBu_W93g_TtCDinZZOf46l2FfzYKl1btdZSaV9IMov2o6h4QgCOzNWcAq_2t6"
+	x_auth_token =  "your-auth-token-here"
 	}
 `
 )
